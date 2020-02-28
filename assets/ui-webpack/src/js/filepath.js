92e3f9b220382cc3583bdfa806895ab2750b6bbc
dfkdream <dfkdream@gmail.com>
2020-02-28 21:06:51 +0900

    Add abs,clean

--- assets/ui-webpack/src/js/filepath.js
+++ assets/ui-webpack/src/js/filepath.js
@@ -11,12 +11,47 @@ module.exports = {
     },
 
     join: function(u1, u2){
-        if (u1[u1.length-1]!="/"){
+        if (u1[u1.length-1]!=="/"){
             u1+="/";
         }
-        if (u2[0]=="/"){
+        if (u2[0]==="/"){
             u2=u2.slice(1)
         }
         return u1+u2;
+    },
+
+    abs: function(base, rel){
+        let stack = base.split("/"),
+            parts = rel.split("/");
+        stack.pop(); // remove current file name (or empty string)
+                     // (omit if "base" is the current folder without trailing slash)
+        for (let i=0; i<parts.length; i++) {
+            if (parts[i] === ".")
+                continue;
+            if (parts[i] === "..")
+                stack.pop();
+            else
+                stack.push(parts[i]);
+        }
+        return stack.join("/");
+    },
+
+    clean: function(path){
+        let stack = path.split("/");
+        let result = [""];
+
+        stack.forEach(i=>{
+            switch(i){
+                case "": case ".":
+                    return;
+                case "..":
+                    result.pop();
+                    break;
+                default:
+                    result.push(i);
+            }
+        });
+
+        return this.join("/",result.join("/"));
     }
 };
