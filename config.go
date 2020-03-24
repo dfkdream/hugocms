38a63be187a12f831383313859c18cc9bb657cf8
dfkdream <dfkdream@gmail.com>
2020-03-24 17:16:05 +0900

    Add plugin configuration

--- config.go
+++ config.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
+
+	"github.com/dfkdream/hugocms/plugin"
 )
 
 type config struct {
@@ -17,6 +22,7 @@ type config struct {
 	TLS         bool
 	CertPath    string
 	KeyPath     string
+	Plugins     []plugin.Metadata
 }
 
 func (c config) String() string {
@@ -35,10 +41,28 @@ func getConfig() *config {
 		TLS:      getEnvBoolOr("TLS", false),
 		CertPath: getEnvStringOr("CERT", "./cert.pem"),
 		KeyPath:  getEnvStringOr("KEY", "./key.pem"),
+		Plugins:  make([]plugin.Metadata, 0),
 	}
 	cfg.ConfigPath = getEnvStringOr("CONFIG", filepath.Join(cfg.Dir, "config.yaml"))
 	cfg.ContentPath = getEnvStringOr("CONTENT", filepath.Join(cfg.Dir, "/content"))
 	cfg.PublicPath = getEnvStringOr("PUBLIC", filepath.Join(cfg.Dir, "/public"))
+
+	ps := getEnvStringOr("PLUGINS", "")
+	if ps != "" {
+		for _, v := range strings.Split(ps, ",") {
+			pluginAddr := strings.TrimSpace(v)
+			if !strings.HasPrefix(pluginAddr, "http://") {
+				pluginAddr = singleJoiningSlash("http://", pluginAddr)
+				m, err := getPluginMetadata(pluginAddr)
+				if err != nil {
+					log.Fatal(err)
+				}
+				cfg.Plugins = append(cfg.Plugins, *m)
+			}
+
+		}
+	}
+
 	return &cfg
 }
 
@@ -50,8 +74,35 @@ func getEnvStringOr(key string, defaultValue string) string {
 }
 
 func getEnvBoolOr(key string, defaultValue bool) bool {
-	if value, error := strconv.ParseBool(os.Getenv(key)); error != nil {
+	if value, err := strconv.ParseBool(os.Getenv(key)); err != nil {
 		return value
 	}
 	return defaultValue
+}
+
+func singleJoiningSlash(a, b string) string {
+	aSlash := strings.HasSuffix(a, "/")
+	bSlash := strings.HasPrefix(b, "/")
+	switch {
+	case aSlash && bSlash:
+		return a + b[1:]
+	case !aSlash && !bSlash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+func getPluginMetadata(pluginAddr string) (*plugin.Metadata, error) {
+	addr := singleJoiningSlash(pluginAddr, "metadata")
+	res, err := http.Get(addr)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = res.Body.Close() }()
+	m := new(plugin.Metadata)
+	err = json.NewDecoder(res.Body).Decode(m)
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
 }
