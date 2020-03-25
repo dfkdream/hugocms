785b2732755a409778483ad781d0abb618110fed
dfkdream <dfkdream@gmail.com>
2020-03-25 11:41:35 +0900

    Add plugin templates

--- admin.go
+++ admin.go
@@ -11,6 +11,12 @@ import (
 type admin struct {
 	signIn *signInHandler
 	t      *template.Template
+	config *config
+}
+
+type templateVars struct {
+	Plugins []pluginData
+	Body    template.HTML
 }
 
 func (a admin) setupHandlers(router *mux.Router) {
@@ -26,7 +32,7 @@ func (a admin) setupHandlers(router *mux.Router) {
 	})
 
 	router.PathPrefix("/list/").HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		err := a.t.ExecuteTemplate(res, "list.html", nil)
+		err := a.t.ExecuteTemplate(res, "list.html", templateVars{Plugins: a.config.Plugins})
 		if err != nil {
 			log.Println(err)
 			http.Error(res, "Internal Server Error", http.StatusInternalServerError)
@@ -34,7 +40,7 @@ func (a admin) setupHandlers(router *mux.Router) {
 	})
 
 	router.PathPrefix("/edit").HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		err := a.t.ExecuteTemplate(res, "edit.html", nil)
+		err := a.t.ExecuteTemplate(res, "edit.html", templateVars{Plugins: a.config.Plugins})
 		if err != nil {
 			log.Println(err)
 			http.Error(res, "Internal Server Error", http.StatusInternalServerError)
@@ -42,7 +48,7 @@ func (a admin) setupHandlers(router *mux.Router) {
 	})
 
 	router.HandleFunc("/config", func(res http.ResponseWriter, req *http.Request) {
-		err := a.t.ExecuteTemplate(res, "config.html", nil)
+		err := a.t.ExecuteTemplate(res, "config.html", templateVars{Plugins: a.config.Plugins})
 		if err != nil {
 			log.Println(err)
 			http.Error(res, "Internal Server Error", http.StatusInternalServerError)
