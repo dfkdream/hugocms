c27a2f3bc5e646f91499256c8abd18e383b57f2e
dfkdream <dfkdream@gmail.com>
2020-03-25 18:55:17 +0900

    Add plugin list page

---
+++ assets/ui-webpack/src/js/plugins.js
@@ -0,0 +1,12 @@
+/*!
+ * HugoCMS
+ * https://github.com/dfkdream/HugoCMS
+ *
+ * Copyright 2020 dfkdream
+ * Released under MIT License
+ */
+
+require('../css/style.css');
+require('../css/plugins.css');
+
+require('spectre.css/dist/spectre.min.css');
